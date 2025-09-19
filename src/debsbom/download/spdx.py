9acd392156a9ced1aceaea792a28d5304bdafc6b
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-09-19 11:01:02 +0200

    refactor(resolver): add document property and is_debian_package
    By that, we can avoid double parsing in case we need the document in
    later steps again. We further make the is_debian_package part of the
    public API to also make it reusable.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- src/debsbom/download/spdx.py
+++ src/debsbom/download/spdx.py
@@ -33,7 +33,24 @@ class SpdxPackageResolver(PackageResolver, SPDXType):
     def package_manager_ref(cls, p: spdx_package.Package) -> spdx_package.ExternalPackageRef | None:
         cat_pkg_manager = spdx_package.ExternalPackageRefCategory.PACKAGE_MANAGER
         return next(
-            filter(lambda ref: ref.category == cat_pkg_manager, p.external_references), None
+            filter(lambda ref: ref.category == cat_pkg_manager, p.external_references), None)
+
+    @staticmethod
+    def is_debian_pkg(p):
+        if not p.external_references:
+            return False
+        # TODO: scan all references
+        if (
+            p.external_references[0].category
+            != spdx_package.ExternalPackageRefCategory.PACKAGE_MANAGER
+        ):
+            return False
+        return True
+
+    def debian_pkgs(self):
+        return map(
+            lambda p: self.package_from_purl(p.external_references[0].locator),
+            filter(self.is_debian_pkg, self._document.packages),
         )
 
     @classmethod

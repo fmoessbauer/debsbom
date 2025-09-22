85227ab1945546cd59310fb11c14ec4b90bfb55f
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-09-22 13:04:01 +0200

    chore(dpkg): add merge_with method to enrich package data
    Merging packages is a common task. By making the merging a method of the
    package, we only need to maintain that logic in one place and can keep
    it in sync with the classes members.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- src/debsbom/dpkg/package.py
+++ src/debsbom/dpkg/package.py
@@ -117,6 +117,12 @@ class Package(ABC):
                     seen.add(k)
                 yield element
 
+    def merge_with(self, other: "Package"):
+        if not self.maintainer:
+            self.maintainer = other.maintainer
+        if not self.homepage:
+            self.homepage = other.homepage
+
     # TODO: check type
     @classmethod
     def _resolve_sources(cls, pkg: "BinaryPackage", add_pkg=False) -> Iterable["Package"]:
@@ -166,7 +172,7 @@ class SourcePackage(Package):
         name: str,
         version: str | Version,
         maintainer: str | None = None,
-        binaries: list[str] | None = None,
+        binaries: list[str] = [],
         homepage: str | None = None,
         vcs_browser: str | None = None,
         vcs_git: str | None = None,
@@ -214,6 +220,17 @@ class SourcePackage(Package):
             version_wo_epoch = self.version.upstream_version
         return f"{self.name}_{version_wo_epoch}.dsc"
 
+    def merge_with(self, other: "SourcePackage"):
+        super().merge_with(other)
+        if not self.vcs_browser:
+            self.vcs_browser = other.vcs_browser
+        if not self.vcs_git:
+            self.vcs_git = other.vcs_git
+        # add binaries from other
+        binaries = list(self.binaries)
+        binaries.extend(x for x in other.binaries if x not in binaries)
+        self.binaries = binaries
+
     @staticmethod
     def from_dep822(package) -> "SourcePackage":
         name = package["Package"]
@@ -222,7 +239,7 @@ class SourcePackage(Package):
         if package.get("Binaries") is not None:
             binaries = [b.strip() for b in package["Binaries"].split(",")]
         else:
-            binaries = None
+            binaries = []
         homepage = package.get("Homepage")
         vcs_browser = package.get("Vcs-Browser")
         vcs_git = package.get("Vcs-Git")
@@ -309,6 +326,28 @@ class BinaryPackage(Package):
     @locator.setter
     def locator(self, loc) -> None:
         self._locator = loc
+
+    def merge_with(self, other: "BinaryPackage"):
+        super().merge_with(other)
+        if not self.section:
+            self.section = other.section
+        if not self.architecture:
+            self.architecture = other.architecture
+        if not self.source:
+            self.source = other.source
+        if not self.description:
+            self.description = other.description
+        if not self.checksums:
+            self.checksums = other.checksums
+        self.manually_installed |= other.manually_installed
+        if not self.depends:
+            depends = list(self.depends)
+            depends.extend(x for x in other.binaries if x not in depends)
+            self.depends = depends
+        if not self.built_using:
+            built_using = list(self.built_using)
+            built_using.extend(x for x in other.binaries if x not in built_using)
+            self.built_using = built_using
 
     @staticmethod
     def from_dep822(package) -> "BinaryPackage":

3e5b1e3bbc76038bf325aca5fcde88ceef9bcc4a
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-09-23 15:45:09 +0200

    feat(download): compare checksums of downloaded data with SBOM
    In case we have checksums of components in the SBOM, compare these with
    the checksums of the downloaded artifacts. By that, we can detect cases
    where a package was locally altered and rebuilt without changing the
    version.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- src/debsbom/cli.py
+++ src/debsbom/cli.py
@@ -217,7 +217,7 @@ class DownloadCmd:
                 DownloadCmd._check_for_dsc(pkg, files)
             except sdlclient.NotFoundOnSnapshotError:
                 logger.warning(f"not found upstream: {pkg.name}@{pkg.version}")
-            downloader.register(files)
+            downloader.register(files, pkg)
 
         nfiles, nbytes, cfiles, cbytes = downloader.stat()
         print(

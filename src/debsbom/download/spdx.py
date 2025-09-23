3895b97da38f0fd89495805bb406ab63dec0de1d
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-09-23 13:28:17 +0200

    chore(download): read package checksum on resolving
    When converting sbom packages into our internal package representation,
    read the checksum values as well. By that, we can later use these values
    to compare the observed checksum with the one we get when resolving.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- src/debsbom/download/spdx.py
+++ src/debsbom/download/spdx.py
@@ -3,13 +3,25 @@
 # SPDX-License-Identifier: MIT
 
 from collections.abc import Iterable
-from ..dpkg.package import Package
+import logging
+from ..dpkg.package import ChecksumAlgo, Package
 from ..sbom import SPDXType
 from .download import PackageResolver
 from pathlib import Path
 from spdx_tools.spdx.parser.parse_anything import parse_file
 import spdx_tools.spdx.model.package as spdx_package
 import spdx_tools.spdx.model.document as spdx_document
+from spdx_tools.spdx.model.checksum import ChecksumAlgorithm
+
+
+logger = logging.getLogger(__name__)
+
+
+CHKSUM_TO_INTERNAL = {
+    ChecksumAlgorithm.MD5: ChecksumAlgo.MD5SUM,
+    ChecksumAlgorithm.SHA1: ChecksumAlgo.SHA1SUM,
+    ChecksumAlgorithm.SHA256: ChecksumAlgo.SHA256SUM,
+}
 
 
 class SpdxPackageResolver(PackageResolver, SPDXType):
@@ -31,9 +43,19 @@ class SpdxPackageResolver(PackageResolver, SPDXType):
             return True
         return False
 
+    @classmethod
+    def create_package(cls, p: spdx_package.Package) -> Package:
+        pkg = cls.package_from_purl(cls.package_manager_ref(p).locator)
+        for cks in p.checksums:
+            if cks.algorithm not in CHKSUM_TO_INTERNAL.keys():
+                logger.debug(f"ignoring unknown checksum on {pkg.name}@{pkg.version}")
+                continue
+            pkg.checksums[CHKSUM_TO_INTERNAL[cks.algorithm]] = cks.value
+        return pkg
+
     def debian_pkgs(self) -> Iterable[Package]:
         return map(
-            lambda p: self.package_from_purl(self.package_manager_ref(p).locator),
+            lambda p: self.create_package(p),
             filter(self.is_debian_pkg, self._document.packages),
         )
 
